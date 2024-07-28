e92b1c8e03088ca711efa161ad3900aa29a7a697
Jules Rotschi <rotschi.jules@gmail.com>
2024-07-28 17:32:35 +0200

    Fix login usecases return type

---
+++ src/domain/contracts/dto/auth/token_output_dto.ts
@@ -0,0 +1,3 @@
+export interface TokenOutputDTO {
+  token: string
+}
--- src/domain/usecases/auth/login_usecase.ts
+++ src/domain/usecases/auth/login_usecase.ts
@@ -1,13 +1,14 @@
 import { inject } from "@adonisjs/core";
 import { AuthRepository } from "#contracts/repositories/auth_repository";
 import { LoginUserInputDTO } from "#contracts/dto/user/login_user_input_dto";
+import { TokenOutputDTO } from "#contracts/dto/auth/token_output_dto";
 
 @inject()
 export class LoginUsecase {
 
   constructor(private authRepository: AuthRepository) {}
 
-  async handle(user: LoginUserInputDTO) {
+  async handle(user: LoginUserInputDTO): Promise<TokenOutputDTO> {
     return {
       token: await this.authRepository.getToken(user.uid, user.password)
     };
